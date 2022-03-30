60614ee7973fda3185a6f358faee1f1037d97a9a
RajarsiMukherjee <96103218+RajarsiMukherjee@users.noreply.github.com>
2022-03-30 10:16:51 +0530

    first

---
+++ rajtwitter/src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Explore } from "./pages/Explore";
+
+function App() {
+  return (
+    <div className="App">
+       
+        <Explore />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ rajtwitter/src/Configs/firebase.jsx
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAYXqvDnEdfp9dcqCb70cToekInaeo5aOA",
+  authDomain: "twitter-clone-ba49b.firebaseapp.com",
+  projectId: "twitter-clone-ba49b",
+  storageBucket: "twitter-clone-ba49b.appspot.com",
+  databaseURL: "https://twitter-clone-ba49b.firebaseio.com",
+  messagingSenderId: "1041126736507",
+  appId: "1:1041126736507:web:e8251e40e1eb00a36b98de",
+  measurementId: "G-MEFDQRTKFF"
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+const db = firebaseApp.firestore();
+
+export default db;
